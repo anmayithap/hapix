fe71e407fe67e1c3a591573a6e07d0d05de7ad08
Ruslan Torbeev <ruslan.torbeev@yandex.ru>
2026-05-09 21:16:50 +0300

    feat(packages): Add `bat` wrapped package

---
+++ modules/packages/bat.nix
@@ -0,0 +1,22 @@
+# -----------------------------------------------------------------------------
+# ## [PACKAGES -> bat] A wrapped cat(1) clone
+# -----------------------------------------------------------------------------
+#: Source: https://github.com/sharkdp/bat
+#: Changelog: https://github.com/sharkdp/bat/blob/master/CHANGELOG.md
+{self, ...}: {
+  perSystem = {
+    pkgs,
+    lib,
+    ...
+  }: let
+    D = lib.mkDefault;
+  in {
+    packages.bat = self.wrappers.bat.wrap {
+      inherit pkgs;
+
+      bat = {
+        style = D "auto";
+      };
+    };
+  };
+}
