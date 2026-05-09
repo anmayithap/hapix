8c102f8510df29c9c57f73d2f7cca93e68e34351
Ruslan Torbeev <ruslan.torbeev@yandex.ru>
2026-05-09 23:21:16 +0300

    feat(packages): Add `starship` wrapped package

---
+++ modules/packages/starship.nix
@@ -0,0 +1,23 @@
+# -----------------------------------------------------------------------------
+# ## [PACKAGES -> starship] cross-shell prompt
+# -----------------------------------------------------------------------------
+#: Source: https://github.com/starship/starship
+#: Documentation: https://starship.rs/config/
+#: Changelog: https://github.com/starship/starship/blob/main/CHANGELOG.md
+{self, ...}: {
+  perSystem = {
+    lib,
+    pkgs,
+    ...
+  }: let
+    B = lib.mkBefore;
+  in {
+    packages.starship = self.wrappers.starship.wrap {
+      inherit pkgs;
+
+      preset = B [
+        "nerd-font-symbols"
+      ];
+    };
+  };
+}
